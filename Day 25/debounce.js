d4cf682106e9ce9cb6012b155b1b82c547cf1cbd
Jenifer29b <Jenifer29b@gmail.com>
2025-12-02 19:47:14 +0530

    tasks of Day 25

--- Day 25/debounce.js
+++ Day 25/debounce.js
@@ -100,3 +100,29 @@ debounced("search")
 debounced("searchi")
 debounced("searchin")
 debounced("searched")
+
+function debounce(fn,delay,immediate) {
+    let timeout;
+     
+    return function (...args) {
+        let context = this
+        clearTimeout(timeout)
+
+        if (immediate == true && !timeout) {
+             fn.apply(context,args)
+        } 
+            timeout = setTimeout(() => {
+             if (!immediate) fn.apply(context, args)
+             timeout = null
+        }, delay);
+        
+         
+    }
+}
+function repeat(value) {
+    console.log(value)
+}
+
+let debounced1 = debounce(repeat, 2000,true)
+debounced1("fool")
+debounced1("foolished")
--- Day 25/tasks.js
+++ Day 25/tasks.js
@@ -1,25 +1,98 @@
-function debounce(fn,delay,immediate) {
-    let timeout;
-     
+function debounce(fn, delay) {
+  let timeout;
+
+  return function (...args) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
+let debounced = debounce(console.log, 2000);
+debounced("ab");
+debounced("abc");
+debounced("abcd");
+debounced("abcde");
+
+function debounce1(fn,delay) {
+    let timeout 
+
     return function (...args) {
-        let context = this
+        clearTimeout(timeout)
+
+        timeout = setTimeout(() => {
+            fn.apply(this,args)
+        }, delay);
+    }
+    
+}
+
+function search(value) {
+    console.log("Searched for :",value)
+}
+let searched = debounce1(search, 1000)
+searched("se")
+searched("sea")
+searched("sear")
+searched("search")
+
+let desres = debounce1(console.log, 3000)
+
+for (let index = 0; index < 5; index++) {
+    desres("loop")
+    
+}
+
+
+function deboun(fn, delay,immediate) {
+    let timeout 
+
+    return function (...args) {
         clearTimeout(timeout)
 
         if (immediate == true && !timeout) {
-             fn.apply(context,args)
-        } 
-            timeout = setTimeout(() => {
-             if (!immediate) fn.apply(context, args)
-             timeout = null
-        }, delay);
-        
-         
+            fn.apply(this,args)
+        }
+
+        timeout = setTimeout(() => {
+           if(!immediate) fn.apply(this, args)
+            timeout = null
+        },delay)
     }
 }
-function repeat(value) {
-    console.log(value)
-}
 
-let debounced = debounce(repeat, 2000,true)
-debounced("fool")
-debounced("foolished")
+let res = deboun(console.log, 4000, false)
+res("First")
+res("Second")
+res("Third")
+res("Fourth")
+
+
+function throttle(fn, delay) {
+    let timelaps = 0;
+
+    return function (...args) {
+        let now = Date.now()
+
+        if (now - timelaps >= delay) {
+            fn.apply(this, args)
+            timelaps = now;
+        }
+    }
+}
+let result = throttle(console.log, 2000)
+result("A")
+result("B")
+result("C")
+result(setTimeout(() => {
+    console.log("D")
+}, 2500))
+
+let result2 = throttle(console.log, 3000)
+
+for (let index = 0; index < 5; index++) {
+   result2(index)
+    
+}
